779f60ed87361ae650f3ee6bbc4f427e3d9a8e83
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-03-27 22:18:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var name = "Susan"
-var height = "70"
+var height = "74"
+if (height === "70")
+{
+  console.log("The height is correct")
+} else {
+  console.log("Expected the height to be 74")
+}
 
 // Don't worry about this
 module.exports = { name, height
