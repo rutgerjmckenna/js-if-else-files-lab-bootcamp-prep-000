70af6c697db7acf5540e8b1bccad9afbecee030d
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-03-27 22:20:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ if (height === "70")
 {
   console.log("The height is correct")
 } else {
-  console.log("Expected the height to be 74")
+  console.log("Expected the height to be 70")
 }
 
 // Don't worry about this
